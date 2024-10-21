6c640245c70fefcdbb3939f15edb6846e9d7b7a7
Ivanandres05 <ivanandreshernandezc@gmail.com>
2024-10-21 12:48:57 -0500

    Add files via upload
    seguimiento 1 del corete 2

---
+++ ejercicio6.js
@@ -0,0 +1,12 @@
+let nombre = prompt("Ingrese nombre"); 
+let idioma = prompt("Ingrese idioma");  
+
+const saludarPersona = (nombre, idioma = "es") => {
+    if (idioma === "es") {
+        return "Hola, " + nombre;
+    } else if (idioma === "en") {
+        return "Hello, " + nombre;
+    }
+    return "Idioma no soportado";
+}
+alert(saludarPersona(nombre, idioma))
